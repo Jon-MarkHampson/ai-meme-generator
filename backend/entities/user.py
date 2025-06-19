87e4b8e2e6aeb23e15300b150cdf464a9a83214d
Jon-Mark Hampson <jonmarkhampson@me.com>
2025-06-19 14:18:33 +0200

    feat(user): enhance created_at field with timezone-aware datetime

--- backend/entities/user.py
+++ backend/entities/user.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 from datetime import datetime, timezone
 from sqlmodel import SQLModel, Field
+from sqlalchemy import Column, DateTime
 
 
 class User(SQLModel, table=True):
@@ -12,7 +13,12 @@ class User(SQLModel, table=True):
     email: str = Field(index=True, nullable=False, unique=True)
     hashed_password: str = Field(nullable=False)
     created_at: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc), nullable=False
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(
+            DateTime(timezone=True),
+            default=lambda: datetime.now(timezone.utc),
+            nullable=False,
+        ),
     )
 
     def __repr__(self):
