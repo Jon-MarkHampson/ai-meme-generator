be78923bf7864f078502243a02695c5a509c920c
Jon-Mark Hampson <jonmarkhampson@me.com>
2025-07-05 08:34:01 +0200

    fix(api): correct comment on withCredentials for clarity

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8000",
-  withCredentials: true, // browser will include your HttpOnly access_token cookie
+  withCredentials: true, // browser will include HttpOnly access_token cookie
 });
 
 export default API;
