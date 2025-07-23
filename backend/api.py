4074c7101442ec5288ea8cff7fb7dfb4ce6f7bef
Jon-Mark Hampson <jonmarkhampson@me.com>
2025-07-23 15:21:19 +0200

    feat: remove unused routers from API registration

--- backend/api.py
+++ backend/api.py
@@ -9,9 +9,6 @@ from fastapi import FastAPI
 from features.auth.controller import router as auth_router
 from features.users.controller import router as user_router
 from features.image_storage.controller import router as upload_image_router
-from features.caption_requests.controller import router as caption_requests_router
-from features.caption_variants.controller import router as caption_variants_router
-from features.image_variants.controller import router as image_variants_router
 from features.user_memes.controller import router as user_memes_router
 from features.generate.controller import router as generate_router
 from features.llm_providers.controller import router as llm_providers_router
@@ -26,9 +23,6 @@ def register_routers(app: FastAPI):
     app.include_router(auth_router)
     app.include_router(user_router)
     app.include_router(upload_image_router)
-    app.include_router(caption_requests_router)
-    app.include_router(caption_variants_router)
-    app.include_router(image_variants_router)
     app.include_router(user_memes_router)
     app.include_router(generate_router)
     app.include_router(llm_providers_router)
